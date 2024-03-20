64e12eb1e625d01f0ddcc8e5e8522461ca5a5fc2
seoyoung <915jin@gmail.com>
2024-03-20 12:50:56 +0900

    express 셋팅

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-var http=require('http');  //노드모듈을 가져오기
-var hostname='127.0.0.1'  //내 컴퓨터 내부주소
+const express=require("express");
+const cors=require("cors");
+const app=express();
 var port="8080" //사용할 서버 포토 번호
 
+app.use(express.json());
+app.use(cors());
 //   서버생성(request: 요청, responsive: 응답)
-const server=http.createServer(function(req, res){
-    const path=req.url;
-    const method=req.method;
-    if(path==='/products'){
-        if(method==='GET'){
-            res.writeHead(200,{"Content-Type":"application/json"});
-            const products=JSON.stringify([
-                {
-                    name:"강아지 물병", 
-                    price:6000,
-                    
-                },
-            ]);
-            res.end(products)
-        }else if(method==='POST'){
-            res.end("생성되었습니다.")
-
-        }
-        
-    }
-    res.end("bye bye")
+app.get('/products', (req, res)=>{
+    res.send({
+        products:[
+            {id:0,name:'스크래쳐 캣 오르간',price:16100, seller:"에구에구", imageUrl:'img/products/toy01.jpg'},
+            {id:1,name:'네이처 써클 캣토이',price:16300, seller:"티티캣", imageUrl:'img/products/toy02.jpg'},
+            {id:2,name:'셀프오뚜기',price:13700, seller:"힐링타임", imageUrl:'img/products/toy03.jpg'},
+            {id:3,name:'고양이 스크래쳐',price:13600, seller:"우프우프", imageUrl:'img/products/toy04.jpg'},
+        ]
+    })
 })
-server.listen(port, hostname);
-console.log('server 성공')
+app.get('/products/:id/events/:eventId', (req, res)=>{
+    const params=req.params;
+    const {id, eventId} = params;
+    res.send(`id는 ${id}와 ${eventId}입니다.`)
+
+
+    /* const query=req.query;
+    console.log('Query:', query) */
+})
+
+app.post('/products', (req, res)=>{
+    const body=req.body;
+    res.send({
+        body:body
+    })
+})
+app.post('/login',(req, res)=>{
+    res.send('로그인해주세요.')
+})
+
+
+app.listen(port, ()=>{
+    console.log('펫 서버가 돌아가고 있습니다.')
+})
