9629861fac21cb9ae5fe3512ba15aca91d5b67d2
seoyoung <915jin@gmail.com>
2024-03-22 13:51:59 +0900

    오류확인용

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
-const models=require('./models')
-var port="8080" //사용할 서버 포토 번호
+const models=require('./models');
+const multer= require("multer");
+const upload=multer({
+    storage:multer.diskStorage({
+        destination:function(req, file, cb){
+            cb(null, "uploads/") 
+        },
+        filename:function(req, file, cb){
+            cb(null, file.originalname)
+        }
+    })
+})
+var port="8080" //사용할 서버 포트
+
 
 app.use(express.json());
 app.use(cors());
-//   서버생성(request: 요청, responsive: 응답)
-app.get('/products', (req, res)=>{
+app.use("/uploads", express.static("uploads"))
+//서버생성(request: 요청 , responsive: 응답)
+app.get('/products', (req, res) =>{
     models.Product.findAll({
         order:[
             ['createdAt', 'DESC']
-            // ASC:오름차순, DESC:내림차순       
+            //ASC:오름차순, DESC:내림차순
         ],
-        attributes:["id","name","description", "seller", "createdAt", "imageUrl", "price"]
-        
+        attributes:["id","name","description","price","seller","createdAt","imageUrl"],
     })
     .then((result)=>{
-        console.log("PRODUCT :", result);
+        console.log("PRODUCT : ", result);
         res.send({
-            product: result,
+            product:result,
         })
     })
     .catch((error)=>{
         console.error(error);
-        res.send("에러발생")
+        res.status(400).send("에러발생")
     })
 })
-app.get('/products/:id', (req, res)=>{
+app.get('/products/:id', (req, res) => {
     const params=req.params;
     const {id} = params;
     models.Product.findOne({
@@ -36,49 +48,51 @@ app.get('/products/:id', (req, res)=>{
         }
     })
     .then((result)=>{
-        console.log("product: ", result);
+        console.log("product:", result);
         res.send({
             product: result
         })
     })
     .catch((error)=>{
         console.error(error);
-        res.send('상품 조회시 에러가 발생했습니다')
+        res.status(400).send('상품조회시 에러가 발생했습니다.')
     })
 })
-
-app.post('/products', (req, res)=>{
+app.post('/products', (req, res) =>{
     const body=req.body;
-    const {name, price, seller, description} = body;
-    if(!name || !price || !seller || !description){
+    const {name, price, seller, description, imageUrl} =body;
+    if(!name || !price || !seller || !description || !imageUrl){
         res.send('모든 필드를 입력해주세요')
     }
 
     models.Product.create({
-        name, 
+        name,
         description,
         price,
-        seller
+        seller,
+        imageUrl,
+
     })
     .then((result)=>{
-        console.log('상품생성결과:',result);
+        console.log('상품생성결과:', result);
         res.send({result,})
     })
     .catch((error)=>{
         console.error(error)
-        res.send('상품업로드에 문제가 생겼습니다.')
+        res.status(400).send('상품 업로드에 문제가 발생했습니다.')
     })
-     /* res.send({
-        body:body
-    })  */
+    
 })
-app.post('/login',(req, res)=>{
-    res.send('로그인해주세요.')
+app.post('/image', upload.single('image'), (req, res) =>{
+    const file=req.file;
+    console.log(file)
+    res.send({
+       imageurl:file.path, 
+    })
 })
 
-
-app.listen(port, ()=>{
-    console.log('펫 서버가 돌아가고 있습니다.')
+app.listen(port, () =>{
+    console.log('펫 서버가 돌아가고 있습니다. ')
     models.sequelize.sync()
     .then(()=>{
         console.log('DB연결 성공')
